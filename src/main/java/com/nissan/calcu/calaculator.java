ff0c1cffe33560120042b710cf2d6bdd36ee98ce
samakshjain04 <samj16@ymail.com>
2018-08-17 12:47:22 +0530

    fixed compilation issues pom warning

--- src/main/java/com/nissan/calcu/calaculator.java
+++ src/main/java/com/nissan/calcu/calaculator.java
@@ -1,12 +1,13 @@
 package com.nissan.calcu;
 
 public class calaculator {
+	int x=10;
 	int sum(int a,int b)
 	{
-		return(a+b);
+		return a+b;
 	}
 	int sub(int a,int b)
 	{
-		return(a-b);
+		return a-b;
 	}
 }
