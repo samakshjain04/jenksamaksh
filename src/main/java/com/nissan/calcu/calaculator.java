55ed20efa3e6e6f8175bb92a0049012cb84a2ea7
samakshjain04 <samj16@ymail.com>
2018-08-17 12:05:48 +0530

    calc

---
+++ src/main/java/com/nissan/calcu/calaculator.java
@@ -0,0 +1,12 @@
+package com.nissan.calcu;
+
+public class calaculator {
+	int sum(int a,int b)
+	{
+		return(a+b);
+	}
+	int sub(int a,int b)
+	{
+		return(a-b);
+	}
+}
---
+++ src/test/java/com/nissan/calcu/TestCalculator.java
@@ -0,0 +1,25 @@
+package com.nissan.calcu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import calci.Calcu;
+
+public class TestCalculator {
+
+
+		calaculator c=new calaculator();
+		
+		@Test
+		public void sumtest() {
+			assertEquals(7,c.sum(5, 2));
+		}
+		
+		@Test
+		public void subtest() {
+			assertEquals(3,c.sub(5, 2));
+		}
+	
+
+}
